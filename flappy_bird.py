90f3154ee4d616888ff8c377149fc8743835b18c
Deanit <deanitz@gmail.com>
2019-11-07 22:18:24 +0300

    naive implemetation shows that it doesnt want to learn so ez

--- flappy_bird.py
+++ flappy_bird.py
@@ -7,18 +7,91 @@ import numpy as np
 class AI:
     def __init__(self):
         self.data = list()
+        self.answers = list()
 
+        self.learn_rate = 0.0005
+        self.hidden_size = 12
+        self.input_size = 10
+
+        self.weights_01 = 2 * np.random.random((self.input_size, self.hidden_size)) - 1
+        self.weights_12 = 2 * np.random.random((self.hidden_size,1)) - 1
+
+        self.activation_func = self.tanh
+        self.activation_func_deriv = self.tanh2deriv
+        self.output_func = lambda x: x
+        self.error_func = self.avg_square_error
+
+        
     def addGameData(self, new_data):
         self.data.append(new_data)
-        sys.stdout.write("                                           \r"+ str(new_data))
-        self.printAiInfo()
+        
+        #sys.stdout.write("                                           \r"+ str(new_data))
+        #self.printAiInfo()
+
+        prog = self.makeProg(new_data)
+        self.answers.append(prog)
+        return prog
+    
+    def makeProg(self, data):
+        layer_0 = np.array(data)
+        layers = self.calcLayers(layer_0)
+        return layers[1]
+
+    def calcLayers(self, layer_0):
+        layer_1 = self.activation_func(np.dot(layer_0, self.weights_01))
+        layer_2 = self.output_func(np.dot(layer_1, self.weights_12))
+        return (layer_1,layer_2)
     
     def printAiInfo(self):
         print(len(self.data))
 
     def iterateCycle(self, is_dead):
+        self.trainBatch(not is_dead)
         self.data.clear()
-        print(is_dead)
+        self.answers.clear()
+        print("dead" if is_dead else "alive")
+
+    def trainBatch(self, is_positive):
+        layer_2_error = 0
+        
+        for i in range(len(self.answers)):
+            layer_0 = np.array(self.data[i:i+1])
+            layers =  self.calcLayers(layer_0)
+            layer_1 = layers[0]
+            layer_2 = layers[1]
+            
+            goal = self.answers[i:i+1][0]
+            goal = goal if is_positive else (-goal)
+
+            layer_2_error += self.error_func(layer_2, [goal])
+            
+            #производная от среднеквадратического отклонения - скаляр
+            layer_2_delta = layer_2 - goal 
+            
+            #back propagation func:
+            #поэлементное произведение весов последнего скрытого слоя (рез. - вектор длины скрытого слоя)
+            #умноженное на производную от функции активации
+            layer_1_delta = layer_2_delta * self.weights_12.T * self.activation_func_deriv(layer_1)
+            
+            error_power = 2 if i == 0 else (1 / np.sqrt(i))
+
+            self.weights_12 -= self.learn_rate * layer_1.T.dot(layer_2_delta) * error_power
+            self.weights_01 -= self.learn_rate * layer_0.T.dot(layer_1_delta) * error_power
+
+        print("error: " + str(layer_2_error))
+    
+    def tanh(self,x):
+        return np.tanh(x)
+
+    def tanh2deriv(self,output):
+        return 1 - (output ** 2)
+
+    def softmax(self,x):
+        temp = np.exp(x)
+        return temp / np.sum(temp, axis=0, keepdims=True)
+
+    def avg_square_error(self,np_arr, goal):
+        return np.sum((np_arr - goal) ** 2)
 
 
 class FlappyBird:
@@ -46,6 +119,7 @@ class FlappyBird:
         self.frame = 0
         self.iteration = 0
         self.prevGameInfo = self.getGameInfoForAi()
+        self.lastAiCommand = 0.
 
     def updateWalls(self):
         self.wallx -= 2
@@ -98,8 +172,8 @@ class FlappyBird:
         
         if(self.frame%5==0 and not self.dead):
             new_info = self.getGameInfoForAi()
-            gameInfo = [self.prevGameInfo, new_info]
-            self.ai.addGameData(gameInfo)
+            gameInfo = self.prevGameInfo + new_info
+            self.lastAiCommand = self.ai.addGameData(gameInfo)
             self.prevGameInfo = new_info
             
 
@@ -115,13 +189,22 @@ class FlappyBird:
 
             self.updateAi()
 
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            """
                 if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
                     self.jump = 17
                     self.gravity = 5
                     self.jumpSpeed = 10
+
+            """
+            if(self.lastAiCommand > 0.5 and not self.dead):
+                self.jump = 17
+                self.gravity = 5
+                self.jumpSpeed = 10
+            
 
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.background, (0, 0))
