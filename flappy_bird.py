3728ce2f151c38d44a4324ab0d472cd6d10bb0a8
Deanit <deanitz@gmail.com>
2019-11-08 00:13:35 +0300

    tries to fly without obstacles

--- flappy_bird.py
+++ flappy_bird.py
@@ -9,12 +9,14 @@ class AI:
         self.data = list()
         self.answers = list()
 
-        self.learn_rate = 0.0005
-        self.hidden_size = 12
-        self.input_size = 10
+        self.learn_rate = 0.011
+        self.hidden1_size = 48
+        self.hidden2_size = 128
+        self.input_size = 6
 
-        self.weights_01 = 2 * np.random.random((self.input_size, self.hidden_size)) - 1
-        self.weights_12 = 2 * np.random.random((self.hidden_size,1)) - 1
+        self.weights_01 = 2 * np.random.random((self.input_size, self.hidden1_size)) - 1
+        self.weights_12 = 2 * np.random.random((self.hidden1_size,self.hidden2_size)) - 1
+        self.weights_23 = 2 * np.random.random((self.hidden2_size,1)) - 1
 
         self.activation_func = self.tanh
         self.activation_func_deriv = self.tanh2deriv
@@ -35,50 +37,68 @@ class AI:
     def makeProg(self, data):
         layer_0 = np.array(data)
         layers = self.calcLayers(layer_0)
-        return layers[1]
+        return layers[-1]
 
     def calcLayers(self, layer_0):
         layer_1 = self.activation_func(np.dot(layer_0, self.weights_01))
-        layer_2 = self.output_func(np.dot(layer_1, self.weights_12))
-        return (layer_1,layer_2)
+        layer_2 = self.activation_func(np.dot(layer_1, self.weights_12))
+        layer_3 = self.output_func(np.dot(layer_2, self.weights_23))
+        return (layer_1,layer_2,layer_3)
     
     def printAiInfo(self):
-        print(len(self.data))
+        return
+        #print(len(self.data))
 
     def iterateCycle(self, is_dead):
         self.trainBatch(not is_dead)
         self.data.clear()
         self.answers.clear()
-        print("dead" if is_dead else "alive")
+        #print("dead" if is_dead else "alive")
 
     def trainBatch(self, is_positive):
-        layer_2_error = 0
+        layer_3_error = 0
         
-        for i in range(len(self.answers)):
+        batch_len = len(self.answers)
+        error_power = (1 / np.log(batch_len**2))
+        endorse_power = np.log(batch_len) if is_positive else 1
+        for i in range(batch_len):
             layer_0 = np.array(self.data[i:i+1])
             layers =  self.calcLayers(layer_0)
             layer_1 = layers[0]
-            layer_2 = layers[1]
-            
-            goal = self.answers[i:i+1][0]
-            goal = goal if is_positive else (-goal)
 
-            layer_2_error += self.error_func(layer_2, [goal])
+            if (not is_positive):
+                dropout_mask = np.random.randint(2,size=layer_1.shape)
+                layer_1 *= dropout_mask * 2
+
+            layer_2 = layers[1]
+            if (not is_positive):
+                dropout_mask = np.random.randint(2,size=layer_2.shape)
+                layer_2 *= dropout_mask * 2
+
+            layer_3 = layers[2]
+
+            goal = self.answers[i:i+1][0]
+            goal = goal if is_positive else (-goal) * error_power
+
+            layer_3_error += self.error_func(layer_3, [goal])
             
             #производная от среднеквадратического отклонения - скаляр
-            layer_2_delta = layer_2 - goal 
+            layer_3_delta = layer_3 - goal 
             
             #back propagation func:
             #поэлементное произведение весов последнего скрытого слоя (рез. - вектор длины скрытого слоя)
             #умноженное на производную от функции активации
-            layer_1_delta = layer_2_delta * self.weights_12.T * self.activation_func_deriv(layer_1)
+            layer_2_delta = layer_3_delta * self.weights_23.T * self.activation_func_deriv(layer_2)
+
+            layer_1_delta = layer_2_delta.dot(self.weights_12.T.dot(self.activation_func_deriv(layer_1.T)))
             
-            error_power = 2 if i == 0 else (1 / np.sqrt(i))
+            
+            self.weights_23 -= self.learn_rate * layer_2.T.dot(layer_3_delta) * endorse_power
+            self.weights_12 -= self.learn_rate * layer_1.T.dot(layer_2_delta) * endorse_power
+            self.weights_01 -= self.learn_rate * layer_0.T.dot(layer_1_delta) * endorse_power
 
-            self.weights_12 -= self.learn_rate * layer_1.T.dot(layer_2_delta) * error_power
-            self.weights_01 -= self.learn_rate * layer_0.T.dot(layer_1_delta) * error_power
-
-        print("error: " + str(layer_2_error))
+        #print(self.answers)
+        #print("error: " + str(layer_3_error))
     
     def tanh(self,x):
         return np.tanh(x)
@@ -120,6 +140,7 @@ class FlappyBird:
         self.iteration = 0
         self.prevGameInfo = self.getGameInfoForAi()
         self.lastAiCommand = 0.
+        self.framerate = 60
 
     def updateWalls(self):
         self.wallx -= 2
@@ -140,6 +161,7 @@ class FlappyBird:
             self.birdY += self.gravity
             self.gravity += 0.2
         self.bird[1] = self.birdY
+        """
         upRect = pygame.Rect(self.wallx,
                              360 + self.gap - self.offset + 10,
                              self.wallUp.get_width() - 10,
@@ -150,10 +172,13 @@ class FlappyBird:
                                self.wallDown.get_height())
         if upRect.colliderect(self.bird) or downRect.colliderect(self.bird):
             self.dead = True
+        """
         if not 0 < self.bird[1] < 720:
             self.bird[1] = 50
             self.birdY = 50
             self.dead = False
+            if self.counter > 0:
+                print("score: " + str(self.counter))
             self.counter = 0
             self.wallx = 400
             self.offset = random.randint(-110, 110)
@@ -167,10 +192,10 @@ class FlappyBird:
 
     def updateAi(self):
         self.frame += 1
-        if (self.frame >= 60):
+        if (self.frame >= self.framerate):
             self.frame = 0
         
-        if(self.frame%5==0 and not self.dead):
+        if((self.frame % 5) == 0 and not self.dead):
             new_info = self.getGameInfoForAi()
             gameInfo = self.prevGameInfo + new_info
             self.lastAiCommand = self.ai.addGameData(gameInfo)
@@ -178,14 +203,14 @@ class FlappyBird:
             
 
     def getGameInfoForAi(self):
-        return [self.birdY / 1000, (self.wallx + 200) / 1000,  (self.offset + 100) / 200, self.gravity / 10, self.jump / 20]
+        return [self.birdY / 1000, (self.wallx + 200) / 1000,  (self.offset + 100) / 200]
 
     def run(self):
         clock = pygame.time.Clock()
         pygame.font.init()
         font = pygame.font.SysFont("Arial", 50)
         while True:
-            clock.tick(60)
+            clock.tick(self.framerate)
 
             self.updateAi()
 
@@ -200,7 +225,7 @@ class FlappyBird:
                     self.jumpSpeed = 10
 
             """
-            if(self.lastAiCommand > 0.5 and not self.dead):
+            if(self.lastAiCommand > 0 and not self.dead):
                 self.jump = 17
                 self.gravity = 5
                 self.jumpSpeed = 10
