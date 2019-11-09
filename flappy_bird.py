59a1836753d1fe1f005fd5e432ea85184d973532
Deanit <deanitz@gmail.com>
2019-11-10 01:00:46 +0300

    collecting successful flies data, then training on them in one batch

--- flappy_bird.py
+++ flappy_bird.py
@@ -13,9 +13,9 @@ class AI:
         self.answers = list()
 
         self.hidden1_size = 60
-        self.hidden2_size = 300
-        self.hidden3_size = 18
-        self.input_size = 30
+        self.hidden2_size = 128
+        self.hidden3_size = 7
+        self.input_size = 40
 
         self.createNewAi()
 
@@ -24,12 +24,23 @@ class AI:
         self.output_func = lambda x: x
         self.error_func = self.avg_square_error
 
+        self.epochs_count = 300
+        self.epoch_size = 5000
+        self.successful_batches = deque(maxlen=self.epoch_size)
+        
+
     def createNewAi(self):
-        self.learn_rate = 0.00005
+        self.learn_rate = 0.00001
+        self.avg_error = 0
+        self.max_score = 0
+
+        self.teach_tries = 1
+
         self.weights_01 = 2 * np.random.random((self.input_size, self.hidden1_size)) - 1
-        self.weights_12 = 2 * np.random.random((self.hidden1_size,self.hidden2_size)) - 1
-        self.weights_23 = 2 * np.random.random((self.hidden2_size,self.hidden3_size)) - 1
-        self.weights_34 = 2 * np.random.random((self.hidden3_size,1)) - 1
+        self.weights_12 = 0.5 * np.random.random((self.hidden1_size,self.hidden2_size)) - 0.25
+        self.weights_23 = 0.5 * np.random.random((self.hidden2_size,self.hidden3_size)) - 0.25
+        self.weights_34 = 0.2 * np.random.random((self.hidden3_size,1)) - 0.1
+        print()
         print("creating new AI")
 
         
@@ -45,6 +56,10 @@ class AI:
     
     def makeProg(self, data):
         layer_0 = np.array(data)
+        if(len(data) == 0):
+            print()
+            print("oops!prog")
+            return 1
         layers = self.calcLayers(layer_0)
         return layers[-1]
 
@@ -56,31 +71,65 @@ class AI:
         return (layer_1,layer_2,layer_3,layer_4)
     
     def printAiInfo(self):
+        sys.stdout.write("                                                                                                                  \r"\
+            +"SBL: " + str(len(self.successful_batches))\
+                +" | MAX: " + str(self.max_score)\
+                    +" | ERR: " + str(self.avg_error)\
+                )
         return
-        #print(len(self.data))
 
     def iterateCycle(self, is_dead, score):
-        self.trainBatch(not is_dead, score)
+        self.printAiInfo()
+
+        if(len(self.data) == 0):
+            print()
+            print("oops!train")
+            return
+            
+        self.trainBatch(not is_dead, score, self.data, self.answers)
+
+        # if is_dead and len(self.successful_batches) >= 30:
+        #     for _ in range(self.teach_tries):
+        #         for (success_data, success_answer) in self.successful_batches:
+        #             self.trainBatch(False, 0.1, success_data[10::2], success_answer[10::2])
+
+        if score > 1:
+            self.successful_batches.append( (self.data.copy(), self.answers.copy()) )
+
+        if len(self.successful_batches) >= self.epoch_size:
+            print("big training started!")
+            for _ in range(self.epochs_count):
+                for (success_data, success_answer) in self.successful_batches:
+                    self.trainBatch(True, 0.1, success_data[10:], success_answer[10:])
+
+            self.successful_batches.clear()
+            print("big training ended!")
+
+            
+
         self.data.clear()
         self.answers.clear()
-        #print("dead" if is_dead else "alive")
 
-    def trainBatch(self, is_positive, score):
+    def trainBatch(self, is_positive, score, train_data, train_answers):
         layer_output_error = 0
         
-        batch_len = len(self.answers)
+        batch_len = len(train_answers)
         batch_len = batch_len if batch_len > 0 else 1
         magic = np.log((batch_len/13)**2)
         magic = magic if magic > 0 else 1
+
+        if score > self.max_score:
+            self.max_score = score
+
         score = 1 + score / 100
-        endorse_power = magic * score if is_positive else score
+        endorse_power = magic * score# if is_positive else score
         
         #print(batch_len)
         #print(error_power)
         #print(endorse_power)
         for i in range(10 if is_positive else 0, batch_len): #10 first are stubbed data
             
-            layer_0 = np.array(self.data[i:i+1])
+            layer_0 = np.array(train_data[i:i+1])
             layers =  self.calcLayers(layer_0)
             layer_1 = layers[0]
 
@@ -100,7 +149,7 @@ class AI:
 
             layer_4 = layers[3]
 
-            goal = self.answers[i:i+1][0]
+            goal = train_answers[i:i+1][0]
             goal = goal if is_positive else (-goal)
 
             layer_output_error += self.error_func(layer_4, [goal])
@@ -118,9 +167,9 @@ class AI:
             amp =  self.learn_rate* endorse_power          
             if (not (is_positive and ((i % 10) != 0))):
                 self.weights_34 -= amp * layer_3.T.dot(layer_4_delta)
-                self.weights_23 -= amp * layer_2.T.dot(layer_3_delta)
-                self.weights_12 -= amp * layer_1.T.dot(layer_2_delta)
-                self.weights_01 -= amp * layer_0.T.dot(layer_1_delta)
+                self.weights_23 -= amp * 0.01 * layer_2.T.dot(layer_3_delta)
+                self.weights_12 -= amp * 0.01 * layer_1.T.dot(layer_2_delta)
+                self.weights_01 -= amp * 0.001 * layer_0.T.dot(layer_1_delta)
                 
 
         # if (is_positive):
@@ -129,6 +178,7 @@ class AI:
 
         #print(self.answers)
         #print("error: " + str(layer_3_error))
+        self.avg_error = np.average([self.avg_error, layer_output_error])
     
     def tanh(self,x):
         return np.tanh(x)
@@ -154,7 +204,7 @@ class FlappyBird:
                             pygame.image.load("assets/dead.png")]
         self.wallUp = pygame.image.load("assets/bottom.png").convert_alpha()
         self.wallDown = pygame.image.load("assets/top.png").convert_alpha()
-        self.gap = 200  #300 ez #130 hard
+        self.gap = 400  #300 ez #130 hard
         self.wallx = 400
         self.birdY = 350
         self.jump = 0
@@ -168,22 +218,28 @@ class FlappyBird:
         self.ai = AI()
         self.frame = 0
         self.iteration = 0
-        initGI = self.getGameInfoForAi()
-        self.prevGameInfo = deque([initGI,initGI,initGI,initGI,initGI,initGI,initGI,initGI,initGI])
+        self.prevGameInfo = deque(maxlen=10)
+        self.resetGameInfo()
         self.lastAiCommand = 0.
         self.framerate = 10000
-        self.maxScore = 0
+        self.avgScore = 0
 
     def updateWalls(self):
         self.wallx -= 2
         if self.wallx < -80:
-            self.wallx = 400
-            self.offset = random.randint(-110, 110)
+            
 
             if not self.dead:
                 self.counter += 1
-                self.maxScore = np.max([self.counter, self.maxScore])
+                if (self.counter % 20 == 0) and self.gap > 130:
+                    self.gap -= 10
+                self.avgScore = np.average([self.counter, self.avgScore])
                 self.iterateAiCycle(False)
+
+                #self.resetGameInfo()
+
+            self.wallx = 400
+            self.offset = random.randint(-110, 110)
 
     def birdUpdate(self):
         if self.jump:
@@ -207,28 +263,36 @@ class FlappyBird:
             self.dead = True
         
         if not 0 < self.bird[1] < 720:
-            self.bird[1] = 50
-            self.birdY = 50
+            self.bird[1] = 350
+            self.birdY = 350
             self.dead = False
-            if self.counter > 0:
-                print("score: " + str(self.counter))
+            # if self.counter > 0:
+            #     print("score: " + str(self.counter))
             self.counter = 0
             self.wallx = 400
             self.offset = random.randint(-110, 110)
             self.gravity = 5
 
             self.iterateAiCycle(True)
+            #self.resetGameInfo()
+            
+    def resetGameInfo(self):
+        self.prevGameInfo.clear()
+        for i in range(self.prevGameInfo.maxlen):
+            self.prevGameInfo.append(self.getGameInfoForAi(i*2))
 
     def iterateAiCycle(self, is_dead):
         self.iteration += 1
-        if (self.maxScore < 30):
+        if self.avgScore < 50 or self.gap > 180:
             self.ai.iterateCycle(is_dead, self.counter)
-        every = 1000
-        if (self.iteration % every == 0) and (self.maxScore < self.iteration / every) and self.maxScore < 30:
-            self.ai.createNewAi()
+        every = 300
+        if ((self.iteration % every == 0) and (self.avgScore < self.iteration / (every * 2))):
+            if (self.iteration > 10000 and self.avgScore < 30) or self.iteration <= 10000:
+                self.ai.createNewAi()
+                self.avgScore = 0
+            else:
+                print("nice AI, continue")
             self.iteration = 0
-            self.maxScore = 0
-        
 
     def updateAi(self):
         self.frame += 1
@@ -236,18 +300,13 @@ class FlappyBird:
             self.frame = 0
         
         if((self.frame % 1) == 0 and not self.dead):
-            new_info = self.getGameInfoForAi()
-            lst = list(self.prevGameInfo)
-            gameInfo = list(np.array(lst).flatten()) + list(new_info)
-            self.prevGameInfo.rotate()
-            self.prevGameInfo.popleft()
+            new_info = self.getGameInfoForAi(0)
             self.prevGameInfo.append(new_info)
-
-            self.lastAiCommand = self.ai.addGameData(gameInfo)
+            self.lastAiCommand = self.ai.addGameData(np.array(list(self.prevGameInfo)).flatten())
             
 
-    def getGameInfoForAi(self):
-        return [self.birdY / 600, (self.wallx + 200) / 1000,  (self.offset + 100) / 200]
+    def getGameInfoForAi(self, frame_off):
+        return [self.birdY / 600, (self.wallx - (frame_off * 2) + 200) / 800,  (self.offset + self.gap / 2 + 100) / 300, (self.offset - self.gap / 2 + 100) / 300, ]
 
     def run(self):
         clock = pygame.time.Clock()
@@ -262,13 +321,19 @@ class FlappyBird:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            """
-                if (event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
-                    self.jump = 17
-                    self.gravity = 5
-                    self.jumpSpeed = 10
 
-            """
+            # if (event.type == pygame.MOUSEBUTTONDOWN) and not self.dead:
+            #     self.jump = 17
+            #     self.gravity = 5
+            #     self.jumpSpeed = 10
+
+            
+            if (event.type == pygame.KEYDOWN):
+                if event.key == pygame.K_UP:
+                    self.framerate = 1000
+                elif event.key == pygame.K_DOWN:
+                    self.framerate = 60
+            
             if(self.lastAiCommand > 0 and not self.dead):
                 self.jump = 17
                 self.gravity = 5
